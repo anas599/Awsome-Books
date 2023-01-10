a9a86e3cffc980972265c1d8c0092a6871131f47
anas599 <anas599@yahoo.com>
2023-01-10 23:01:44 +0300

    Add class in javacript insted of array

--- Assets/JavaScript/booksList copy.js
+++ Assets/JavaScript/booksList copy.js
@@ -1,17 +1,15 @@
-class{
-    
-}
+class AwsomeBooks {
+  booksListArr;
 
-let booksListArr = [];
+  constructor(booksListArr) {
+    this.booksListArr = JSON.parse(localStorage.bookListStorage) || [];
+    this.newBook = document.querySelector('.booksList');
+  }
 
-const newBook = document.querySelector('.booksList');
-const retData = JSON.parse(localStorage.bookListStorage);
-booksListArr = retData;
-
-function displayBooks() {
-  newBook.innerHTML = booksListArr
-    .map(
-      (x) => `
+  display() {
+    this.newBook.innerHTML = this.booksListArr
+      .map(
+        (x) => `
     <section class="booksList">
     <p>${x.title}</p>
     <p>${x.author}</p>
@@ -19,12 +17,34 @@ function displayBooks() {
     <hr></hr>
     <section>
     `,
-    )
-    .join('');
+      )
+      .join('');
+  }
 }
+let myBook = new AwsomeBooks()
+// let booksListArr = [];
 window.addEventListener('load', () => {
   displayBooks();
 });
+
+// const newBook = document.querySelector('.booksList');
+// const retData = JSON.parse(localStorage.bookListStorage);
+// booksListArr = retData;
+
+// function displayBooks() {
+//   newBook.innerHTML = booksListArr
+//     .map(
+//       (x) => `
+//     <section class="booksList">
+//     <p>${x.title}</p>
+//     <p>${x.author}</p>
+//     <button onclick="removeBook('${x.title}', '${x.author}')">remove</button>
+//     <hr></hr>
+//     <section>
+//     `,
+//     )
+//     .join('');
+// }
 // eslint-disable-next-line no-unused-vars
 function addBook() {
   const bookTitle = document.querySelector('.title').value;
