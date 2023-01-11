8cee406d04bb61662a5ee597d2d26c9840557722
anas599 <anas599@yahoo.com>
2023-01-11 19:37:13 +0300

    Added background color changer

--- Assets/JavaScript/booksList-3.js
+++ Assets/JavaScript/booksList-3.js
@@ -1,72 +1,91 @@
 class AwsomeBooks {
-    booksListArr;
+  booksListArr;
 
-    constructor(booksListArr) {
-      this.booksListArr = booksListArr;
-      this.id = booksListArr.length - 1;
-      this.newBook = document.querySelector('.booksList');
-      this.displayBooks();
-      this.addButton = document.querySelector('#add-button');
-      this.addButton.addEventListener('click', () => this.addBook());
+  constructor(booksListArr) {
+    this.booksListArr = booksListArr;
+    this.id = booksListArr.length - 1;
+    this.newBook = document.querySelector('.booksList');
+    this.displayBooks();
+    this.addButton = document.querySelector('#add-button');
+    this.addButton.addEventListener('click', () => this.addBook());
 
-      this.removeButtons = document.querySelectorAll('.remove-button');
+    this.removeButtons = document.querySelectorAll('.remove-button');
 
-      for (let j = 0; j < this.removeButtons.length; j += 1) {
-        this.removeButtons[j].addEventListener('click', () => this.removeBook(this.removeButtons[j].id));
+    for (let j = 0; j < this.removeButtons.length; j += 1) {
+      this.removeButtons[j].addEventListener('click', () => this.removeBook(this.removeButtons[j].id));
+    }
+  }
+
+  changeColor() {
+    for (let i = 0; i < this.booksListArr.length; i += 1) {
+      const bgColor = document.querySelector(`.item-${this.booksListArr[i].id}`);
+      if (i % 2 === 0) {
+        bgColor.style.backgroundColor = 'gray';
+      } else {
+        bgColor.style.backgroundColor = 'white';
       }
     }
+  }
 
-    displayBooks() {
-      this.newBook.innerHTML = this.booksListArr
-        .map(
-          (x) => `
+  displayBooks() {
+    this.newBook.innerHTML = this.booksListArr
+      .map(
+        (x) => `
             <div class="item-${x.id}">
-            <p> "${x.title}" By: ${x.author}</p>
+            <p>Title: ${x.title}</p>
+            <p>Author:${x.author}</p>
             <input type = "button" class = "remove-button" id='button-item-${x.id}' value="remove">        
             </div>
             `,
-        )
-        .join('');
-    }
+      )
+      .join('');
+    this.changeColor();
+  }
 
-    addBook() {
-      const bookTitle = document.querySelector('.title').value;
-      const bookAuthor = document.querySelector('.author').value;
+  addBook() {
+    const bookTitle = document.querySelector('.title').value;
+    const bookAuthor = document.querySelector('.author').value;
 
-      this.id += 1;
-      this.booksListArr.push({ id: this.id, title: bookTitle, author: bookAuthor });
+    this.id += 1;
+    this.booksListArr.push({
+      id: this.id,
+      title: bookTitle,
+      author: bookAuthor,
+    });
 
-      const newItem = document.createElement('div');
-      newItem.className = `item-${this.id}`;
+    const newItem = document.createElement('div');
+    newItem.className = `item-${this.id}`;
+    newItem.innerHTML = `
 
-      newItem.innerHTML = `
       <p>Title: ${bookTitle}</p>
       <p>Author: ${bookAuthor}</p>      
       <input type = "button" class = "remove-button" id='button-item-${this.id}' value="remove">  
      
       
       `;
-      this.newBook.appendChild(newItem);
+    this.newBook.appendChild(newItem);
 
-      const removeButton = document.querySelector(`
+    const removeButton = document.querySelector(`
        #button-item-${this.id}`);
-      removeButton.addEventListener('click', () => this.removeBook(removeButton.id));
+    removeButton.addEventListener('click', () => this.removeBook(removeButton.id));
 
-      const str = JSON.stringify(this.booksListArr);
-      localStorage.setItem('bookListStorage', str);
-    }
+    const str = JSON.stringify(this.booksListArr);
+    localStorage.setItem('bookListStorage', str);
+    this.changeColor();
+  }
 
-    removeBook(itemId) {
-      const id = itemId.split('-')[2];
-      const result = this.booksListArr.filter((item) => item.id !== id);
-      this.booksListArr = result;
-      const str = JSON.stringify(this.booksListArr);
-      localStorage.setItem('bookListStorage', str);
+  removeBook(itemId) {
+    const id = itemId.split('-')[2];
+    const result = this.booksListArr.filter((item) => item.id !== Number(id));
+    this.booksListArr = result;
+    const str = JSON.stringify(this.booksListArr);
+    localStorage.setItem('bookListStorage', str);
 
-      const itemToRemove = document.querySelector(`
+    const itemToRemove = document.querySelector(`
           .item-${id}`);
-      this.newBook.removeChild(itemToRemove);
-    }
+    this.newBook.removeChild(itemToRemove);
+    this.changeColor();
+  }
 }
 
 const retData = JSON.parse(localStorage.bookListStorage);
