dabf127ede10fc9e017e9a09084419c5c97e4251
anas599 <anas599@yahoo.com>
2023-01-11 22:24:25 +0300

    Add navItems javascript to use in single app page

---
+++ Assets/JavaScript/navItems.js
@@ -0,0 +1,22 @@
+const contact = document.querySelector('#contact');
+const addBook = document.querySelector('.input-section');
+const bookList = document.querySelector('.booksList');
+
+// eslint-disable-next-line no-unused-vars
+function showContact() {
+  contact.style.display = 'flex';
+  addBook.style.display = 'none';
+  bookList.style.display = 'none';
+}
+// eslint-disable-next-line no-unused-vars
+function showAddBook() {
+  contact.style.display = 'none';
+  addBook.style.display = 'flex';
+  bookList.style.display = 'none';
+}
+// eslint-disable-next-line no-unused-vars
+function showBooks() {
+  contact.style.display = 'none';
+  addBook.style.display = 'none';
+  bookList.style.display = 'flex';
+}
